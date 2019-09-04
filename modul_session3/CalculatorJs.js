2df42a5d8882938e6699da42e94087e23a2ca402
xuanbach1992 <bachax1992@gmail.com>
2019-09-04 17:09:54 +0700

    may tinh if else

--- modul_session3/CalculatorJs.js
+++ modul_session3/CalculatorJs.js
@@ -1,31 +1,31 @@
-let list = document.getElementsByClassName('btn');
-let x = document.getElementById('num_input1');
-let cont = document.getElementsByClassName('btnEqual');
-let list_toantu= document.getElementsByClassName("toanTu");
-// console.log(resuil);
-for (let i = 0; i < list.length; i++) {
-    // let att = document.createAttribute('onclick');
-    // att.value = "setInput(this.value)";
-    // list[i].setAttributeNode(att);
-    list[i].setAttribute("onclick", "setInput(this.value)");//dung 1 lenhj nay or 3 lenh kia
+let InputNumber = document.getElementById('input_num');
+let Number1, Number2;
+let operator;
+
+function ClickButton(val) {
+    InputNumber.value = InputNumber.value + val;
 }
-for(let i=0;i<list_toantu.length;i++){
-    list_toantu[i].setAttribute("onclick","toantu(this.value)");
+
+function ClickButtonOperator(val) {
+    Number1 = Number(InputNumber.value);
+    document.getElementById('input_num').value = "";
+    operator = val;
 }
-function setInput(a) {
-    let y = x.value;
-    document.getElementById('num_input1').value = y + a;
-}
-function toantu(pheptoan){
-    switch (pheptoan) {
-        case +:
+
+function Result() {
+    Number2 = Number(InputNumber.value);
+    switch (operator) {
+        case "+":
+            document.getElementById('input_num').value = Number1 + Number2;
+            break;
+        case "-":
+            document.getElementById('input_num').value = Number1 - Number2;
+            break;
+        case "*":
+            document.getElementById('input_num').value = Number1 * Number2;
+            break;
+        case "/":
+            document.getElementById('input_num').value = Number1 / Number2;
+            break;
     }
 }
-
-cont[0].onclick = function () {
-    let z = x.value;
-    document.getElementById('num_input1').value = eval(z);
-    // document.getElementById('num_input1').value=y;
-};
-
-
