2324686523eb3355a88535c49fe96a56586c9ce5
xuanbach1992 <bachax1992@gmail.com>
2019-09-06 10:52:34 +0700

    Prime number

--- modul_session4/Songuyento/main.js
+++ modul_session4/Songuyento/main.js
@@ -1,23 +1,27 @@
 let primeNumber = prompt('nhập số các nguyên tố muốn hiển thị, số lượng', '');
-let count1;
-let N = 2;
-for (count1 = 1; count1 <= primeNumber; count1++) {
-    let Numberlet = count1;
-    let songuyento = true;
-    if (Numberlet % 2 == 0) {
-        songuyento = false;
+let dem = 0;
+
+function checkPrimer(val) {
+    if (val == 2) {
+        return true;
+    } else if (val % 2 == 0) {
+        return false;
     } else {
-        for (let i = 3; i <= Math.sqrt(Numberlet); i = i + 2) {
-            if (Numberlet % i == 0) {
-                songuyento = false;
+        for (let i = 3; i <= Math.sqrt(val); i = i + 2) {
+            if (val % i == 0) {
+                return false;
                 break;
             }
-            // console.log(i);
         }
     }
-    if (songuyento) {
-        N = N + primeNumber;
-    }
+    return true;
 }
-alert(N);
-// }
+
+let NumberCount = 2;
+while (dem < primeNumber) {
+    if (checkPrimer(NumberCount)) {
+        dem++;
+        document.write(NumberCount + " ");
+    }
+    NumberCount++;
+}
