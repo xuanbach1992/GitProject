ec9b01f7b0a8b60b34306362940d15f1712ba808
xuanbach1992 <bachax1992@gmail.com>
2019-09-10 17:45:52 +0700

    luyen tap array

--- modul_session5/mainArray.js
+++ modul_session5/mainArray.js
@@ -4,7 +4,7 @@ let MaxArray;
 function CreatArrayRd() {
     Arr = [];
     for (let i = 0; i < 10; i++) {
-        Arr[i] = Math.ceil(Math.random() * 100);
+        Arr[i] = Math.ceil(Math.random() * 100 - 40);
     }
     document.getElementById('demo').innerHTML = "";
     document.getElementById('demo').innerHTML += `[${Arr}]`;
@@ -44,10 +44,69 @@ function NumberMedium() {
 
 function SwapElement() {
 // let tepm;
+    let array4 = []
     for (let first =0,last = Arr.length-1; first < Arr.length/2; first++,last--) {
             let tepm = Arr[first];
            Arr[first] = Arr[last];
            Arr[last]=tepm;
     }
+    // for(let first =0,last = Arr.length-1; first < Arr.length; first++,last--){
+    //     array4[last]=Arr[first];
+    // }
     document.getElementById('demo4').innerHTML = `[${Arr}]`;
+    // document.getElementById('demo4').innerHTML = `[${array4}]`;
+}
+
+function NumberNegative() {
+    let CountNumberNegative = 0;
+    for (let i = 0; i < Arr.length; i++) {
+        if (Arr[i] < 0) {
+            CountNumberNegative++;
+        }
+    }
+    document.getElementById('demo5').innerHTML = `so phan tu nguyen am la ${CountNumberNegative}`;
+}
+
+function NumberCheck() {
+    let checkNumber = Number(prompt('Nhap so muon check'));
+    for (let i = 0; i < Arr.length; i++) {
+        if (checkNumber === Arr[i]) {
+            document.getElementById('demo6').innerHTML = `${checkNumber}  co trong mang`;
+            break;
+        } else {
+            document.getElementById('demo6').innerHTML = `${checkNumber} KHONG co trong mang`;
+        }
+    }
+}
+
+function CheckAndDel() {
+    let checkNumDel = Number(prompt('Nhap so muon xoa'));
+    // if (Arr.includes(checkNumDel)) {
+        for (let i = 0; i < Arr.length-1; i++) {
+            if (checkNumDel === Arr[i]) {
+                let ValueTepm = Arr[i];
+                Arr[i] = Arr[i + 1];
+                Arr[i + 1] = ValueTepm;
+            }
+        }
+    // }
+    Arr.pop();
+    document.getElementById('demo7').innerHTML = `mang moi la: ${Arr}`;
+}
+function SortNumber() {
+    Arr.sort(function (a,b) {
+        return a-b;
+    });
+    document.getElementById('demo8').innerHTML = `mang moi sap xep: ${Arr}`;
+}
+function ArrayPlusArray() {
+    document.getElementById("demo9").innerHTML=`mang 1 la[${Arr}]`+"<br>";
+    let Array2=[];
+    for (let i=0;i<10;i++){
+        let numberInputArray2=+(prompt('Nhap phan tu vap mang moi'));
+        Array2.push(numberInputArray2);
+    }
+     document.getElementById("demo9").innerHTML+=` mang  2 la :[${Arr}]`;
+    let Array3=Arr.concat(Array2);
+    document.getElementById("demo9").innerHTML+=` mang  moi tao tu 2 mang cu  la :[${Array3}]`;
 }
