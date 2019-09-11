70bc368adc49886339dcaa0bdab30128210ca4b1
xuanbach1992 <bachax1992@gmail.com>
2019-09-11 22:55:42 +0700

    chuyen doi met sang feet va nguocj lai

---
+++ modul_session6/FeetMeter/main.js
@@ -0,0 +1,9 @@
+function feetConvertMet(scope) {
+   let meter= scope * 0.305;
+    document.getElementById("meterId").value=meter;
+}
+
+function metConvertFeet(scope) {
+   let feet= scope*3.279;
+    document.getElementById("feetId").value=feet;
+}
