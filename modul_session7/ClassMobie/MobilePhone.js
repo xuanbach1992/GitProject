a7be85c4e4e12cb71b50b3a44404963bdf9b0ef2
xuanbach1992 <bachax1992@gmail.com>
2019-09-16 18:23:34 +0700

    mobile new

--- modul_session7/ClassMobie/MobilePhone.js
+++ modul_session7/ClassMobie/MobilePhone.js
@@ -1,4 +1,4 @@
-const
+// let countId = 0;
 let Mobile = function (name) {//khai bao lop Mobile
 
     this.name = name;
@@ -23,7 +23,22 @@ let Mobile = function (name) {//khai bao lop Mobile
 let nokia = new Mobile("Nokia");//khoi tao lop mobile
 let iphone = new Mobile("IP");
 
-nokia.writeMsg("hello mes");
-nokia.sendMsg(iphone);
-
+// nokia.writeMsg("hello mes");
+// nokia.sendMsg(iphone);
+// function sendMessage(name1,name1) {
+//     name1.sendMsg(name2);
+// }
+function addMobile() {
+    document.getElementById('mobile').innerHTML = `
+    <fieldset>
+    <legend>${nokia.name}</legend>
+    <label>Battery: ${nokia.battery} </label><p></p><br>
+    <input      type="text">
+    <button     type="button">Send</button>
+    <button     type="button">Inbox</button>
+    <button     type="button">Send Inbox</button>
+    <button     type="button">Dralt</button>
+    <br><p></p>
+</fieldset>`;
+}
 
