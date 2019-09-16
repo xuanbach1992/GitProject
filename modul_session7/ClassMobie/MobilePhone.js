09d98f6620e9bff3e55438f7fb83653795760ef3
xuanbach1992 <bachax1992@gmail.com>
2019-09-16 23:04:00 +0700

    demo telephone

--- modul_session7/ClassMobie/MobilePhone.js
+++ modul_session7/ClassMobie/MobilePhone.js
@@ -1,44 +1,78 @@
-// let countId = 0;
-let Mobile = function (name) {//khai bao lop Mobile
-
+let display="";
+let Mobile = function (name) {
     this.name = name;
-    this.battery = 100;
+    this.batery = 100;
     this.inbox = [];
-    this.sent = [];
+    this.outbox = [];
     this.msg = "";
 
     this.writeMsg = function (value) {
         this.msg = value;
     };
     this.sendMsg = function (mobile) {
-        this.sent.push(this.msg);
+        this.outbox.push(this.msg);
         mobile.reveiceMsg(this.msg, this.name);
-    }
-    ;
+    };
+
     this.reveiceMsg = function (msg, name) {
         this.inbox.push(msg);
-        console.log("da nhan tin nhan"+this.inbox+" tu "+name);
     };
 };
-let nokia = new Mobile("Nokia");//khoi tao lop mobile
-let iphone = new Mobile("IP");
+let iphone = new Mobile('IP');
+let nokia = new Mobile('nokia');
 
-// nokia.writeMsg("hello mes");
-// nokia.sendMsg(iphone);
-// function sendMessage(name1,name1) {
-//     name1.sendMsg(name2);
-// }
-function addMobile() {
-    document.getElementById('mobile').innerHTML = `
-    <fieldset>
-    <legend>${nokia.name}</legend>
-    <label>Battery: ${nokia.battery} </label><p></p><br>
-    <input      type="text">
-    <button     type="button">Send</button>
-    <button     type="button">Inbox</button>
-    <button     type="button">Send Inbox</button>
-    <button     type="button">Dralt</button>
-    <br><p></p>
-</fieldset>`;
+function sendMsg(mobile1, mobile2) {
+    mobile1.sendMsg(mobile2);
+    display();
+    // showBtn();
+    mobile1.batery--;
 }
 
+function display() {
+    // document.getElementById("inboxNokia").innerHTML = "";
+    // document.getElementById("inboxNokia").innerHTML += `${nokia.inbox} </br>`;
+
+    document.getElementById('batteryNokia').innerHTML = `Battery:  ${nokia.batery}`;
+
+    // document.getElementById("sentNokia").innerHTML = "";
+    // document.getElementById("sentNokia").innerHTML += `${nokia.outbox} </br>`;
+
+    document.getElementById("inputNokia").value = "";
+
+    // document.getElementById("inboxIphone").innerHTML = "";
+    // document.getElementById("inboxIphone").innerHTML += `${iphone.inbox} </br>`;
+
+    // document.getElementById("sentIphone").innerHTML = "";
+    // document.getElementById("sentIphone").innerHTML += `${iphone.outbox} </br>`;
+
+    document.getElementById('batteryIphone').innerHTML = `Battery:  ${iphone.batery}`;
+
+    document.getElementById("inputIphone").value = "";
+}
+
+function showBtn(nameBtn) {
+    switch (nameBtn) {
+        case "inboxPhone1":
+            document.getElementById("inboxNokia").innerHTML = "";
+            document.getElementById("inboxNokia").innerHTML = nokia.inbox+"</br>";
+            break;
+        case "sentPhone1":
+            document.getElementById("sentNokia").innerHTML = "";
+            document.getElementById("sentNokia").innerHTML += `${nokia.outbox} </br>`;
+            break;
+        case "inboxPhone2":
+            document.getElementById("inboxIphone").innerHTML = "";
+            document.getElementById("inboxIphone").innerHTML += `${iphone.inbox} </br>`;
+            break;
+        case "sentPhone2":
+            document.getElementById("sentIphone").innerHTML = "";
+            document.getElementById("sentIphone").innerHTML += `${iphone.outbox} </br>`;
+            break;
+    }
+}
+
+function printMsg(arr) {
+    for (let i = 0; i <arr.length; i++) {
+        display += `${i + 1}||${arr[i]} <br/>`;
+    }
+}
