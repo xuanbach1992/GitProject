ed4a94cf073426aa90a9624f0c9a250e4bd3d655
xuanbach1992 <bachax1992@gmail.com>
2019-09-17 13:45:17 +0700

    lamp

---
+++ modul_session8/Lamp/classLamp.js
@@ -0,0 +1,56 @@
+let Battery = function () {
+
+    this.setEnergy = function (energy) {
+        this.energy = energy;
+    };
+
+    this.getEnergy = function () {
+        return this.energy;
+    };
+
+    this.decreaseEnergy = function () {
+        if (this.energy > 0) {
+            this.energy--;
+        }
+    };
+};
+
+let FlashLamp = function () {
+    this.setBattery = function (battery) {
+        this._battery = battery;
+    };
+    this.getBatteryInfo = function () {
+        return this._battery.getEnergy()
+    };
+    this.light = function () {
+        if (this._status) {
+            console.log('Den sang');
+        } else {
+            console.log("tat den")
+        }
+    };
+
+    this.turnOn = function () {
+        this._status = true;
+    };
+    this.turnOff = function () {
+        this._status = false;
+    };
+};
+let battery = new Battery();
+battery.setEnergy(10);
+
+let flashLamp = new FlashLamp();
+flashLamp.setBattery(battery);
+
+console.log("Battery info:" + flashLamp.getBatteryInfo() + "<br/>");
+flashLamp.light();
+
+console.log("Turn on<br/>")
+flashLamp.turnOn();
+flashLamp.light();
+console.log("Battery info:" + flashLamp.getBatteryInfo() + "<br/>");
+
+console.log("Turn off<br/>")
+flashLamp.turnOff();
+flashLamp.light();
