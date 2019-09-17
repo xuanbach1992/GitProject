3dca34d9b768259d51c99ec37964cb87b872a300
xuanbach1992 <bachax1992@gmail.com>
2019-09-17 23:44:46 +0700

    move project final

--- modul_session8/MoveProject/script.js
+++ modul_session8/MoveProject/script.js
@@ -1,35 +1,56 @@
-/**
- * Created by nhatnk on 4/26/17.
- */
-
-function Hero(image, top, left, size){
+function Car(image, top, left, size) {
   this.image = image;
   this.top = top;
   this.left = left;
   this.size = size;
 
-  this.getHeroElement = function(){
-    return '<img width="'+ this.size + '"' +
-      ' height="'+ this.size + '"' +
-      ' src="' + this.image +'"' +
-      ' style="top: '+this.top+'px; left:'+this.left+'px;position:absolute;" />';
-  }
-
+  this.setSpeed = function (speed) {
+    this.speed = speed;
+  };
+  this.getSpeed = function () {
+    return this.speed;
+  };
+  this.getHeroElement  = function(){
+    return `<img width="${this.size} "
+        height="${this.size}"
+        src="${this.image}"
+        style="top:${this.top}px;left:${this.left}px;position:absolute;"/>`
+  };
   this.moveRight = function(){
-    this.left += 20;
-    console.log('ok: ' + this.left);
-  }
+    this.left += this.getSpeed();
+    console.log('right: ' + this.left);
+  };
+  this.moveLeft = () => {
+    this.left -= this.getSpeed();
+    console.log('down: ' + this.top);
+  };
+  this.moveUp = () => {
+    this.top -= this.getSpeed();
+    console.log('left: ' + this.left);
+  };
+  this.moveDown = () => {
+    this.top += this.getSpeed();
+    console.log('up: ' + this.top);
+  };
 
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
-
+let car = new Car('Car.png', 0, 0, 100);
+car.setSpeed(5);
+let maxWidth = window.innerWidth - car.size;
+let maxHeight=window.innerHeight-car.size;
 function start(){
-  if(hero.left < window.innerWidth - hero.size){
-    hero.moveRight();
+  if (car.left < maxWidth && car.top === 0) {
+    car.moveRight();
+  } else if (car.left >= maxWidth && car.top < maxHeight) {
+    car.moveDown();
+  } else if (car.left >= 0 && car.top >= maxHeight) {
+    car.moveLeft();
+  } else if (car.left <= 0 && car.top >= 0) {
+    car.moveUp();
   }
-  document.getElementById('game').innerHTML = hero.getHeroElement();
-  setTimeout(start, 500)
+  document.getElementById('game').innerHTML = car.getHeroElement();
+  requestAnimationFrame(start)
 }
 
 start();
