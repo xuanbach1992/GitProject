3837ec75b4db0dbed8ecb3c696e39fb1a7e098b4
xuanbach1992 <bachax1992@gmail.com>
2019-09-18 13:32:48 +0700

    cat and mouse fix mothod

--- modul_session8/Cat&&mouse/catAndMouse.js
+++ modul_session8/Cat&&mouse/catAndMouse.js
@@ -5,7 +5,6 @@ let Mouse = function (name, weight, speed) {
     this.status = true;
 
     this.scream=function () {
-        this.status=true;
         console.log("chit chit");
     };
     this.setName = function (name) {
@@ -14,10 +13,10 @@ let Mouse = function (name, weight, speed) {
     this.getName = function () {
         return this._name;
     };
-    this.set_Weight = function (weight) {
+    this.setWeight = function (weight) {
         this._weight = weight;
     };
-    this.get_Weight = function () {
+    this.getWeight = function () {
         return this._weight;
     };
     this.setSpeed = function (speed) {
@@ -27,11 +26,53 @@ let Mouse = function (name, weight, speed) {
         return this._speed;
     };
     this.checkStatus = function () {
-        if (this.status) {
-            return true;
-        } else {
+        if (!this.status) {
+            console.log("chet roi");
             return false;
+        } else {
+            console.log("song so so ra day");
+            return true;
         }
     }
+};
+let Cat = function (name, weight, speed) {
+    this.name = name;
+    this.weight = weight;
+    this.speed = speed;
 
-}
+    this.scream = function () {
+        console.log("ngaoi ngao");
+    };
+    this.setName = function (name) {
+        this.name = name;
+    };
+    this.getName = function () {
+        return this.name;
+    };
+    this.set_Weight = function (weight) {
+        this.weight = weight;
+    };
+    this.get_Weight = function () {
+        return this.weight;
+    };
+    this.setSpeed = function (speed) {
+        this.speed = speed;
+    };
+    this.getSpeed = function () {
+        return this.speed;
+    };
+    this.eatMouse = function(rat) {
+        this.weight = this.weight + rat._weight;
+    };
+    this.huntingMouse = (rat) => {
+        if (this.speed > rat._speed) {
+            console.log("bat dc chuot");
+        }
+    };
+};
+
+let chuot = new Mouse("Jerry", 2, 4);
+let meo = new Cat("tom", 12, 6);
+meo.huntingMouse(chuot);
+meo.eatMouse(chuot);
+console.log(meo.weight);
