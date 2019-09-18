1bcc6949fbaf68e46fea3bccc8df61d68b723acb
xuanbach1992 <bachax1992@gmail.com>
2019-09-18 09:58:09 +0700

    Electric lamp final

--- modul_session8/ElectricLamp/electricLampClass.js
+++ modul_session8/ElectricLamp/electricLampClass.js
@@ -1,6 +1,70 @@
-let ElectricLamp=function () {
-    this.turnOn=function () {
-        return console.log('den sang');
-    }
+let ElectricLamp = function (status) {
+    this.status = false;
 
+    this.getStatus = function () {
+        return this.status;
+    };
+    this.turnOn=function () {
+        this.status = true;
+        console.log("den bat");
+
+    };
+    this.turnOff = function () {
+        this.status = false;
+        console.log("tat dien");
+    };
+};
+let SwitchButton = function () {
+
+    this.status = false;
+    this.lamp = ElectricLamp;
+
+    this.switchOn = function () {
+        this.status = true;
+        console.log("bat cong tac");
+        this.lamp.turnOn();
+        drawLamp(true);
+    };
+    this.connect = function (lamp1) {
+        this.lamp = lamp1;
+    };
+    this.switchOff = function () {
+        this.status = false;
+        this.lamp.turnOff();
+        console.log("tat cong tac=> tat den");
+        drawLamp(false);
+    };
+
+};
+
+function drawLamp(status) {
+    if (!status){
+        let c=document.getElementById('myCanvas');
+        let ctx=c.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(120,50,40,0,2*Math.PI);
+        ctx.clearRect(0,0,300,150);
+        ctx.strokeStyle="#000000";
+        ctx.stroke();
+    }else {
+        let c=document.getElementById('myCanvas');
+        let ctx=c.getContext("2d");
+        ctx.beginPath();
+        ctx.arc(120,50,40,0,2*Math.PI);
+        ctx.clearRect(0,0,300,150);
+        ctx.fillStyle="yellow";
+        ctx.fill();
+    }
 }
+
+let bongden = new ElectricLamp(true);
+let congtac = new SwitchButton(true);
+let bong2 = new ElectricLamp();
+// bongden.turnOn();
+// bongden.turnOff();
+congtac.connect(bong2);
+// congtac.switchOn();
+
+congtac.switchOff();
+congtac.switchOn();
+
